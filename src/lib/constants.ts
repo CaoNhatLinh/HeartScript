4105406e201c32f5e3187500486cbaf0db8c2837
NhatLinh <caonhatlinh1312@gmai.com>
2026-02-04 10:27:59 +0700

    first commit

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,6 @@
 export const ALLOWED_EMAILS = [
     'caonhatlinh4@gmail.com', // Him (Anh)
-    'caonhatlinh1312@gmail.com' // Her (Em)
+    'ngocnguyenntp2003@gmail.com' // Her (Em)
 ];
 
 export const HIM_EMAILS = ['caonhatlinh4@gmail.com'];
