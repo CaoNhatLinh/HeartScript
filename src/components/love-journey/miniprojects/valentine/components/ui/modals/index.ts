f5b8898e7ca93fe94b9a1f5b9e09edeb9bcdc752
NhatLinh <caonhatlinh1312@gmai.com>
2026-02-13 21:35:50 +0700

    fix: remove dangling MomentsModal export to fix vercel build

--- src/components/love-journey/miniprojects/valentine/components/ui/modals/index.ts
+++ src/components/love-journey/miniprojects/valentine/components/ui/modals/index.ts
@@ -1,6 +1,5 @@
 // Export all modal components
 export { MemoryModal } from './MemoryModal';
-export { MomentsModal } from './MomentsModal'; 
 export { PromiseModal } from './PromiseModal';
 export { PhotoGalleryModal } from './PhotoGalleryModal';
 export { ScreenshotPreview } from './ScreenshotPreview';
